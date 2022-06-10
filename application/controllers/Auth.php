4691a8607394fcee4aaa1475c70285df40963412
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-06-10 22:23:28 +0700

    test 5

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,9 +16,6 @@ class Auth extends BD_Controller
         parent::__construct();
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-        $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
-        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
-        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
         $this->load->model('User_model', 'user');
 
     }
