6fa9d82d2d42c73669684bf90c2ad700c4ae39c2
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-06-10 21:59:07 +0700

    test

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -19,6 +19,8 @@ class Auth extends BD_Controller
         $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+        $this->load->model('User_model', 'user');
+
     }
 
     /**
