6ec5f7ca09d9eee0a25c066f49e562a34280d870
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-06-12 21:44:12 +0700

    commit 7

--- application/models/Question_model.php
+++ application/models/Question_model.php
@@ -61,17 +61,17 @@ class Question_model extends CI_Model
         return $query->result();
     }
 
-    public function add($data, &$error)
+    public function add($data, &$errorMessage)
     {
         $total = 0;
         $result = null;
         $this->db->insert($this->tableName, $data);
         $result = $this->get([$this->question->id => $this->db->insert_id(), $total], null);
         if ($total != null) {
-            $error = "";
+            $errorMessage = "";
             return $result[0];
         } else {
-            $error = $this->db->error(); 
+            $errorMessage = $this->db->error(); 
             return null;
         }
     }
