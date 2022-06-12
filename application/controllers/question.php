c5c42a3e920876f43de7e9f4e78dda18ca0fc6a6
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-06-12 22:02:52 +0700

    test 7

--- application/controllers/question.php
+++ application/controllers/question.php
@@ -114,11 +114,12 @@ class Question extends MY_Controller
     {
 
         $response = null;
-        $messageResult = "";
+        $messageResult = null;
+        $data = null;
         $input = json_decode(trim(file_get_contents('php://input')), true);
         $this->form_validation->set_data($input);
 
-        $data = $this->question->add($input, $messageResult);
+        // $data = $this->question->add($input, $messageResult);
 
         if ($data != null) {
             $response = $this->responses->successWithData($data, 0);
