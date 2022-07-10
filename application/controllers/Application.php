71ad6be27982e1abaa8146294336f09732c77f1e
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-07-11 00:52:14 +0700

    update doc again

--- application/controllers/Application.php
+++ application/controllers/Application.php
@@ -448,7 +448,7 @@ class Application extends MY_Controller
 	 *     @OA\RequestBody(
 	 *     @OA\MediaType(
 	 *         mediaType="application/json",
-	 *         @OA\Schema(ref="#/components/schemas/QuestionInput")
+	 *         @OA\Schema(ref="#/components/schemas/SubmissionDataUpload")
 	 *       ),
 	 *     ),
      * security={{"bearerAuth": {}}},
