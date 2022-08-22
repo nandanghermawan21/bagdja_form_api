651f9037c1e873ebbc80549e164b534f21b50b23
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-08-22 23:52:46 +0700

    commit

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -30,12 +30,12 @@ class MY_Controller extends RestController
 
     parent::__construct();
 
-    // $deviceInfo = $this->_getDeviceInfo();
-    // $supportedAppVersion = $this->config->item('supported_app_version');
-    // $appVersion = explode(" ",$deviceInfo->appVersion)[count(explode(" ",$deviceInfo->appVersion))-1];
-    // if (!isset($supportedAppVersion["1.1.34"])) {
-    //   $this->response("Aplikasi anda sudah tidak didukung  versi app anda saat ini  " . $appVersion . " versi yang didukung " . join(", ", $supportedAppVersion), 500);
-    // }
+    $deviceInfo = $this->_getDeviceInfo();
+    $supportedAppVersion = $this->config->item('supported_app_version');
+    $appVersion = explode(" ",$deviceInfo->appVersion)[count(explode(" ",$deviceInfo->appVersion))-1];
+    if (!isset($supportedAppVersion["1.1.34"])) {
+      $this->response("Aplikasi anda sudah tidak didukung  versi app anda saat ini  " . $appVersion . " versi yang didukung " . join(", ", $supportedAppVersion), 500);
+    }
   }
 
   function _createJWToken($user, $data)
