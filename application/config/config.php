611fdd57dd501c75ad08f022f07d6c005ceb1337
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-08-22 22:47:58 +0700

    add supported version

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ $root  = "https://" . $_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 $config['base_url']    = $root;
 $config['url_login'] = 'https://uat.sfi.co.id/sufismart_ci/TestApi/checklogin';
-$config['supported_app_version'] = array("1.3.33", "1.3.34", "1.3.35");
+$config['supported_app_version'] = array("1.3.33", "1.3.34", "1.3.35", "1.1.34");
 
 /*
 |--------------------------------------------------------------------------
